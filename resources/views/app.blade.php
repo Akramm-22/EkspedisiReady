a1ac02e272e6b2d10870ff29f80a3d9296a000e0
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-17 15:11:16 +0700

    Enable PWA across Inertia pages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#18b378">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="manifest" href="/manifest.json">
-
-    {{-- PWA icon & apple touch icon akan ditambahkan pada modul Offline & PWA --}}
+    <x-pwa-head />
 
     @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
@@ -16,12 +13,6 @@
 <body class="antialiased">
     @inertia
 
-    <script>
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/service-worker.js').catch(console.error);
-            });
-        }
-    </script>
+    <x-pwa-register />
 </body>
 </html>
